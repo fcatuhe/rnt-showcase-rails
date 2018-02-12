c3be01d3412819b7fdc57bbb2b430f279cc3aabe
Francois Catuhe <fcatuhe@gmail.com>
2018-02-12 07:58:15 +0100

    add image to posts with active_storage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,6 +53,6 @@ class PostsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :image)
     end
 end
